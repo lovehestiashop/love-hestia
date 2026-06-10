c7b8b39e7a1ac99bdae6c863c616059d3fb1f39b
lovehestiashop <lovehestiashop@gmail.com>
2026-06-10 17:33:07 +0800

    add to cart page

---
+++ lh-online-ordering/src/pages/cart-page.jsx
@@ -0,0 +1,10 @@
+function CartPage() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Cart Page</h1>
+      <p>Your cart will appear here.</p>
+    </div>
+  );
+}
+
+export default CartPage;
