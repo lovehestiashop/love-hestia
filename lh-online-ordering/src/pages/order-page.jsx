8ea10c287933d04ededfa9af85a020ae8b742c4a
lovehestiashop <lovehestiashop@gmail.com>
2026-06-10 20:48:31 +0800

    order-page.jsx

--- lh-online-ordering/src/pages/order-page.jsx
+++ lh-online-ordering/src/pages/order-page.jsx
@@ -42,7 +42,6 @@ const DateButtonInput = forwardRef(({ value, onClick }, ref) => {
 DateButtonInput.displayName = "DateButtonInput";
 
 function OrderPage() {
-  throw new Error("ORDER PAGE TEST");
   return <h1 style={{fontSize:"50px"}}>TEST ORDER PAGE</h1>;
   const { state } = useLocation();
   alert(JSON.stringify(state));
