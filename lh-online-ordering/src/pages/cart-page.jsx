b3f0e18a1b9d892d2b5d7da95960582212fe61cd
lovehestiashop <lovehestiashop@gmail.com>
2026-06-10 18:19:28 +0800

    /cart

--- lh-online-ordering/src/pages/cart-page.jsx
+++ lh-online-ordering/src/pages/cart-page.jsx
@@ -1,57 +1,33 @@
-import { useEffect, useState } from "react";
+cart.map((item, index) => (
+  <div
+    key={index}
+    style={{
+      marginBottom: "20px",
+      padding: "15px",
+      border: "1px solid #ddd",
+    }}
+  >
+    <img
+      src={
+        item._embedded?.["wp:featuredmedia"]?.[0]
+          ?.media_details?.sizes?.large?.source_url
+      }
+      alt={item.title?.rendered}
+      style={{
+        width: "150px",
+        height: "150px",
+        objectFit: "cover",
+        marginBottom: "10px"
+      }}
+    />
 
-function CartPage() {
-  const [cart, setCart] = useState([]);
+    <h3>{item.title?.rendered}</h3>
 
-  useEffect(() => {
-    const storedCart =
-      JSON.parse(localStorage.getItem("cart")) || [];
-
-    setCart(storedCart);
-  }, []);
-
-  return (
-    <div style={{ padding: "50px" }}>
-      <h1>My Cart</h1>
-
-      {cart.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        cart.map((item, index) => (
-          <div
-            key={index}
-            style={{
-              marginBottom: "20px",
-              padding: "15px",
-              border: "1px solid #ddd",
-            }}
-          >
-            <>
-<img
-  src={
-    item._embedded?.["wp:featuredmedia"]?.[0]
-      ?.media_details?.sizes?.large?.source_url
-  }
-  alt={item.title?.rendered}
-  style={{
-    width: "150px",
-    height: "150px",
-    objectFit: "cover",
-    marginBottom: "10px"
-  }}
-/>
-
-            <p>
-              ₱
-              {Number(
-                item.acf?.price || 0
-              ).toLocaleString("en-PH")}
-            </p>
-          </div>
-        ))
-      )}
-    </div>
-  );
-}
-
-export default CartPage;
+    <p>
+      ₱
+      {Number(
+        item.acf?.price || 0
+      ).toLocaleString("en-PH")}
+    </p>
+  </div>
+))
