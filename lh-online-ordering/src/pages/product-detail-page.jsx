4807a87f2be16f8762981f7ceeba744174d10491
lovehestiashop <lovehestiashop@gmail.com>
2026-06-15 09:37:09 +0800

    Create product-detail-page.jsx

---
+++ lh-online-ordering/src/pages/product-detail-page.jsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+function ProductDetailPage() {
+  const { id } = useParams();
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Product Details</h1>
+      <p>Product ID: {id}</p>
+    </div>
+  );
+}
+
+export default ProductDetailPage;
