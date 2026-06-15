c0845adf04a236438a353baf6dc38f2e45aa964f
lovehestiashop <lovehestiashop@gmail.com>
2026-06-15 09:42:09 +0800

    Update product-detail-page.jsx

--- lh-online-ordering/src/pages/product-detail-page.jsx
+++ lh-online-ordering/src/pages/product-detail-page.jsx
@@ -1,12 +1,45 @@
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function ProductDetailPage() {
-  const { id } = useParams();
+  const { state } = useLocation();
+
+  const product = state?.product;
+
+  if (!product) {
+    return <div>Product not found.</div>;
+  }
+
+  const imageUrl =
+    product._embedded?.["wp:featuredmedia"]?.[0]
+      ?.source_url;
 
   return (
     <div style={{ padding: "50px" }}>
-      <h1>Product Details</h1>
-      <p>Product ID: {id}</p>
+      <img
+        src={imageUrl}
+        alt={product.title.rendered}
+        style={{
+          width: "400px",
+          maxWidth: "100%",
+        }}
+      />
+
+      <h1>{product.title.rendered}</h1>
+
+      <h2>
+        ₱
+        {Number(
+          product.acf?.price || 0
+        ).toLocaleString("en-PH")}
+      </h2>
+
+      <p>
+        Stock: {product.acf?.stock}
+      </p>
+
+      <p>
+        {product.acf?.product_description}
+      </p>
     </div>
   );
 }
