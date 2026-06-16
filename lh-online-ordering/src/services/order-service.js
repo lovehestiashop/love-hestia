d1b4cfbe97ccb2eb98211c393cf2613154071cec
lovehestiashop <lovehestiashop@gmail.com>
2026-06-16 16:27:35 +0800

    Update order-service.js

--- lh-online-ordering/src/services/order-service.js
+++ lh-online-ordering/src/services/order-service.js
@@ -177,13 +177,19 @@ if (
 // Cart checkout
 if (orderData.cart?.length) {
   for (const item of orderData.cart) {
+
+    console.log("Cart item:", item);
+    console.log("Item ID:", item.id);
+
+    if (!item.id) {
+      console.error("Missing product ID", item);
+      continue;
+    }
+
     const newStock = Math.max(
       0,
       Number(item.acf?.stock || 0) - 1,
     );
-
-    console.log("Cart item:", item);
-    console.log("Item ID:", item.id);
 
     await productService.updateStock(
       item.id,
