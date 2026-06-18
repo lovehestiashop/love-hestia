f2860ea4526c643791d13d6942d53189b08c3817
lovehestiashop <lovehestiashop@gmail.com>
2026-06-19 07:27:22 +0800

    Update product-service.js

--- lh-online-ordering/src/services/product-service.js
+++ lh-online-ordering/src/services/product-service.js
@@ -1,39 +1,24 @@
 import api from "./api";
-import axios from "axios";
 
-// --- Get JWT Token ---
-export async function getToken() {
-  try {
-    const res = await axios.post(
-      `${import.meta.env.VITE_API_BASE_URL}/jwt-auth/v1/token`,
-      {
-        username: "admin",
-        password: "admin",
+export const productService = {
+  async getAll() {
+    const res = await api.get("/wp/v2/product", {
+      params: {
+        _embed: true,
+        per_page: 100,
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    });
 
-    return res.data.token;
-  } catch (err) {
-    console.error(
-      "JWT Token Error:",
-      err.response?.data || err.message
-    );
-    throw err;
-  }
-}
+    return res.data;
+  },
 
-async getAll() {
-  const res = await api.get("/wp/v2/product", {
-    params: {
-      _embed: true,
-      per_page: 100,
-    },
-  });
+  async getById(id) {
+    const res = await api.get(`/wp/v2/product/${id}`, {
+      params: {
+        _embed: true,
+      },
+    });
 
-  return res.data;
-},
+    return res.data;
+  },
+};
