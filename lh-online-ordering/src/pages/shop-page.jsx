b4c81baae09c73554c310685026c174a72ac4e32
lovehestiashop <lovehestiashop@gmail.com>
2026-07-07 11:31:09 +0800

    Update shop-page.jsx

--- lh-online-ordering/src/pages/shop-page.jsx
+++ lh-online-ordering/src/pages/shop-page.jsx
@@ -8,13 +8,13 @@ function ShopPage() {
     {
       title: "Flower Bouquets",
       image:
-        "https://api.lovehestia.shop/wp-content/uploads/2026/06/IMG_9440.jpeg",
+        "https://api.lovehestia.shop/wp-content/uploads/2026/07/Flower-Bouquet.jpeg",
       productKey: 5,
     },
     {
       title: "Vase Arrangements",
       image:
-        "https://api.lovehestia.shop/wp-content/uploads/2026/06/IMG_9439.jpeg",
+        "https://api.lovehestia.shop/wp-content/uploads/2026/07/Vase-arrnagements.jpeg",
       productKey: 6,
     },
     {
