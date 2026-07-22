9b676bad8b1c96c0606abbddf0dbde79b0ad6c74
lovehestiashop <lovehestiashop@gmail.com>
2026-07-22 09:48:53 +0800

    Shop page

--- lh-online-ordering/src/pages/shop-page.jsx
+++ lh-online-ordering/src/pages/shop-page.jsx
@@ -20,7 +20,7 @@ function ShopPage() {
     {
       title: "Flower Dome",
       image:
-        "https://api.lovehestia.shop/wp-content/uploads/2026/01/DSC02766.jpg",
+        "https://api.lovehestia.shop/wp-content/uploads/2026/04/IMG_0566-scaled.jpeg",
       productKey: 7,
     },
   ];
